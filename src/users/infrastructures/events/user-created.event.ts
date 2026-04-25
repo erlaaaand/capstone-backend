9e5bb4a196023bbde08424e6e363e1519df71615
erlaaaand <erlandagsya2@gmail.com>
2026-04-25 17:18:34 +0700

    move code

--- src/users/infrastructures/events/user-created.event.ts
+++
@@ -1,9 +0,0 @@
-// src/users/infrastructures/events/user-created.event.ts
-export class UserCreatedEvent {
-  constructor(
-    public readonly userId: string,
-    public readonly email: string,
-    public readonly fullName: string | null,
-    public readonly occurredAt: Date,
-  ) {}
-}
